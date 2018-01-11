65c58a176c89f5d3a90ed0556ea9b4fafc982f75
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-01-11 16:57:06 +0530

    Last Zero Zero !

---
+++ Compiler Design/LastZeroZero/src/com/tyagi/dfa/ZeroZero/LastZeroZero.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.tyagi.dfa.ZeroZero;
+
+import java.util.Scanner;
+
+/**
+ * @author mr tyagi
+ *
+ */
+public class LastZeroZero {
+
+	/**
+	 * 
+	 */
+	public LastZeroZero() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int last;
+		String str1,str2;
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter The Size Of String");
+	    int size= s.nextInt();/*Integer.parseInt(args[0]);*/          
+	    int arg[]=new int[size];
+	    
+	    System.out.println("Enter An String Over Alphabet 0 and 1 Only");
+	    
+	    for(int i=0;i<size;i++) 
+	     {
+	      System.out.println("Enter element :"+i);
+	      arg[i]=s.nextInt();	      
+	     }
+	    if(arg[size-1]==0){
+	    
+	    	if(arg[size-1]==0){
+	    		System.out.println("The String Is Accepted and The Accepted String Is");
+	    	     for(int i=0;i<size;i++)
+	    	      {
+	    	       System.out.println("arg["+i+"]:"+arg[i]);
+	    	      }
+	    	     
+	    	     System.out.println("The Last Two Digits Of String Is");
+	    	     for(int i=size-2;i<size;i++)
+	    	      {
+	    	       System.out.println("arg["+i+"]:"+arg[i]);
+	    	      }
+	    	      
+	    	}
+	    }
+	    else{
+	    	System.out.println("String Is Not Accepted");
+	    }	    
+	}
+
+}
