7b34daf8390245807c6652c24e678d904108b333
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-08-27 21:39:40 +0530

    Update easing.js

--- Web Technology/js/easing.js
+++ Web Technology/js/easing.js
@@ -133,6 +133,7 @@ jQuery.extend( jQuery.easing,
 			return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
 		}
 	},
+	
 	easeInOutBounce: function (x, t, b, c, d) {
 		if (t < d/2) return jQuery.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
 		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
